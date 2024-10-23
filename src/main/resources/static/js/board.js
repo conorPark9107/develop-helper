3b7f8370470e229a371cee1652a2c7b3f6b48096
conorPark9107 <conorpark@naver.com>
2024-10-23 21:02:44 +0900

    게시판 목록(1), 게시판 쓰기(1) 화면단 구현

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -1,4 +1,36 @@
 $(document).ready(function(){
+    const quill = new Quill('#editor', {
+       modules: {
+            toolbar: [
+                  ['bold', 'italic', 'underline'],
+                  ['image'],
+                  ['blockquote'],
+            ],
+      },
+      placeholder: '여기부터 글 작성..',
+      theme: 'snow',
+    });
+
+
+    $('.submit').on('click', function(){
+        const delta = quill.getContents();
+        const html = quill.root.innerHTML.trim();
+        console.log(delta);
+        console.log(html);
+        quill.root.innerHTML = html + '<p>테스트하는 중입니다</p>';
+    });
+
+    $('.cancel').on('click', function(){
+        console.log('취소버튼');
+    });
+
 
 
 });
+
+function clicked_Category(li){
+    $('.category').css('font-size', '0.5em');
+    $('.category').css('color', '');
+    $(li).css('color', 'RGB(28, 120, 153)');
+    $(li).css('font-size', '1em');
+}
