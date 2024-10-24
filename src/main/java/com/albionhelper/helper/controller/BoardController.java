f6ab00d1e8271f777a722b5464929dc078c7f0a5
conorPark9107 <conorpark@naver.com>
2024-10-24 21:42:19 +0900

    게시판 쓰기(2) 구현, 게시글 서버단 구현

--- src/main/java/com/albionhelper/helper/controller/BoardController.java
+++ src/main/java/com/albionhelper/helper/controller/BoardController.java
@@ -2,9 +2,12 @@ package com.albionhelper.helper.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/board")
@@ -20,6 +23,21 @@ public class BoardController {
     @GetMapping("/write")
     public String boardWrite(){
         return "board/write";
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public String boardWriteRegister(@RequestParam("title")String title,
+                                     @RequestParam("password")String pw,
+                                     @RequestPart(value = "images", required = false)List<MultipartFile> images){
+        log.info("title : {}", title);
+        log.info("pw : {} ", pw);
+        log.info(images.get(0).getOriginalFilename());
+        log.info(images.get(1).getOriginalFilename());
+
+        // 이 파일들을 스토리지로 보낸 후 유니크한 파일명을 통해서 다시 바디에 써준다.
+
+        return "nice";
     }
 
 
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
       theme: 'snow',
     });
 
+    quill.on('text-change', function() {
+        document.getElementById("hidden_input").value = quill.root.innerHTML;
+    });
 
     $('.submit').on('click', function(){
         const delta = quill.getContents();
@@ -18,15 +21,88 @@ $(document).ready(function(){
         console.log(delta);
         console.log(html);
         quill.root.innerHTML = html + '<p>테스트하는 중입니다</p>';
+
+        var img = $('img'); // 이미지들.
+        var title = $('#title').val();
+        var password = $('#password').val();
+
+        if(title == ''){
+            $('.title-warning-message').show();
+            setTimeout(() => {
+              $('.title-warning-message').delay(3000).hide();
+            }, 3000);
+            return;
+        }
+        if(password == ''){
+             $('.password-warning-message').show();
+             setTimeout(() => {
+               $('.password-warning-message').delay(3000).hide();
+             }, 3000);
+             return;
+        }
+
+        const formData = new FormData();
+        formData.append('title', title);
+        for(var i = 0; i < img.length; i++){
+            formData.append('images', base64toFile(img[i].src, i));
+        }
+        formData.append('password', password);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "/board/register",
+            data: formData,
+            processData: false,
+            contentType: false,
+            cache: false,
+            success: (data) => {
+                alert("yes : " + data);
+            },
+            error: function(xhr, status, error) {
+                alert(xhr.responseText);
+            }
+        });
+
+
+
+
     });
 
     $('.cancel').on('click', function(){
-        console.log('취소버튼');
+        $.confirm({
+            theme: 'supervan',
+            title: '',
+            content: '글 작성을 취소하시겠습니까?',
+            buttons: {
+                '네': function () {
+                    javascript:history.go(-2);
+                },
+                '아니오': function () {
+
+                }
+            }
+        });
     });
 
 
-
 });
+
+function base64toFile(base_data, filename) {
+
+    var arr = base_data.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, {type:mime});
+}
+
 
 function clicked_Category(li){
     $('.category').css('font-size', '0.5em');
