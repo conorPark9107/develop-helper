e3a6e0fa6d7bc1deb1b7538cfdc7621fdd2faea6
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-02-10 19:33:12 +0900

    Participant 부분 삭제.

--- src/main/java/com/albionhelper/helper/repository/ParticipantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.albionhelper.helper.repository;
-
-import com.albionhelper.helper.domain.battleapi.Participant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-}
