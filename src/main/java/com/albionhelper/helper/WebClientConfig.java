7b797f36a1c035e003ada6636af5cf52b7b179b2
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-02-26 22:00:30 +0900

    웨폰 티어 리스트 페이지 작성 (5), webClient timeout 설정

--- src/main/java/com/albionhelper/helper/WebClientConfig.java
+++ src/main/java/com/albionhelper/helper/WebClientConfig.java
@@ -2,8 +2,12 @@ package com.albionhelper.helper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
@@ -16,6 +20,9 @@ public class WebClientConfig {
                 .build();
 
         WebClient webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create().responseTimeout(Duration.ofSeconds(120))
+                ))
                 .exchangeStrategies(exchangeStrategies)
                 .build();
 
