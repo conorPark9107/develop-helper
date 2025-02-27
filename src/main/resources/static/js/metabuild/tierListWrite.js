72169a5beb4ffee77859a5564735cf3cc4d51d50
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-02-27 20:42:46 +0900

    웨폰 티어 리스트 페이지 작성 (6)

--- src/main/resources/static/js/metabuild/tierListWrite.js
+++ src/main/resources/static/js/metabuild/tierListWrite.js
@@ -1,4 +1,13 @@
-const tier = ['S', 'A', 'B', 'C', 'D', 'E', 'F', 'Z'];
+const tier = {
+    'S' : ['red', 0], 
+    'A' : ['red', 0], 
+    'B' : ['red', 0], 
+    'C' : ['red', 0], 
+    'D' : ['red', 0], 
+    'E' : ['red', 0],  
+    'F' : ['red', 0],  
+    'Z' : ['red', 0], 
+};
 const maxTier = tier.length;
 
 function addTier(){
