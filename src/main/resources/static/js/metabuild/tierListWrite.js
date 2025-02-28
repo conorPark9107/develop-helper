155375b40b617586bb926893de73b01cea7e0217
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-02-28 20:52:30 +0900

    웨폰 티어 리스트 페이지 작성 (7)

--- src/main/resources/static/js/metabuild/tierListWrite.js
+++ src/main/resources/static/js/metabuild/tierListWrite.js
@@ -1,15 +1,31 @@
-const tier = {
-    'S' : ['red', 0], 
-    'A' : ['red', 0], 
-    'B' : ['red', 0], 
-    'C' : ['red', 0], 
-    'D' : ['red', 0], 
-    'E' : ['red', 0],  
-    'F' : ['red', 0],  
-    'Z' : ['red', 0], 
-};
+let itemList = null;
+const tier = [
+    ['S', 0], 
+    ['A', 1], 
+    ['B', 2], 
+    ['C', 3], 
+    ['D', 4], 
+    ['E', 5],  
+    ['F', 6],  
+    ['Z', 7], 
+];
 const maxTier = tier.length;
+let index = 0;
 
-function addTier(){
-    
+fetch('/static/jsonData/items.json')
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.log(error));
+
+
+
+function addTier(){    
+    if(index >= 0 && index < tier.length){
+        const id = tier[index][0];
+        const tierElement = document.getElementById(id);
+        tierElement.className += ' show';
+        index++;
+    }else if(index == tier.length){
+        showAlert('더 이상 추가할 수 없습니다.')
+    }
 }
