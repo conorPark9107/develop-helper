17cc7428e2b997a9f60f8adfcdfaba9642be3968
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-05-16 20:01:03 +0900

    item정보 관련 구조 생성

--- src/main/java/com/albionhelper/helper/controller/ItemInfoConroller.java
+++ src/main/java/com/albionhelper/helper/controller/ItemInfoConroller.java
@@ -1,5 +1,6 @@
 package com.albionhelper.helper.controller;
 
+import com.albionhelper.helper.service.ItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +11,20 @@ public class ItemInfoConroller {
     // 아이템 정보 요청 url
     // https://gameinfo.albiononline.com/api/gameinfo/items/아이템명/data
 
+    private final ItemService itemService;
+
+    public ItemInfoConroller(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping("")
     public String showPage(){
         return "item/item";
     }
+
+    
+
+
 
 
 }
---
+++ src/main/java/com/albionhelper/helper/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.albionhelper.helper.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+
+}
