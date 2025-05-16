7aa7dde6b6b21345e2f6ba8285f444469a177669
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-05-16 20:01:56 +0900

    item정보 관련 구조 생성

--- src/main/java/com/albionhelper/helper/service/ItemService.java
+++ src/main/java/com/albionhelper/helper/service/ItemService.java
@@ -1,9 +1,16 @@
 package com.albionhelper.helper.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class ItemService {
+
+    private final WebClient webClient;
+
+    public ItemService(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
 
 }
