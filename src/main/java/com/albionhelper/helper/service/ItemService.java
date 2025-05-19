1f29e129f0e1e7b07bce6aaa0131724c26e3b440
박종복 <52990763+conorPark9107@users.noreply.github.com>
2025-05-19 18:04:48 +0900

    .

--- src/main/java/com/albionhelper/helper/service/ItemService.java
+++ src/main/java/com/albionhelper/helper/service/ItemService.java
@@ -8,9 +8,14 @@ public class ItemService {
 
     private final WebClient webClient;
 
+    // 장비 아이템의 경우 E스킬(세번째 스킬의 스킬명과 스킬 설명으로 주어짐)
+    // 음식의 경우 : buffOverTime 리스트 필드 아래에 value값이 존재하는데 이게 버프의 값임.
+
     public ItemService(WebClient webClient) {
         this.webClient = webClient;
     }
+
+
 
 
 }
