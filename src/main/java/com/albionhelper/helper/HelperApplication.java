23b408a5ca29aabc063c5ed94ca2e4ad49eb74a2
conorPark9107 <conorpark@naver.com>
2026-03-17 21:22:30 +0900

    BattleService > BattleBoardHttpClient - cacheable 적용 코드 추가.

--- src/main/java/com/albionhelper/helper/HelperApplication.java
+++ src/main/java/com/albionhelper/helper/HelperApplication.java
@@ -2,10 +2,12 @@ package com.albionhelper.helper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class HelperApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HelperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelperApplication.class, args);
+    }
 }
